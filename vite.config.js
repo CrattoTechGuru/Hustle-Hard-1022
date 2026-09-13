5a9e8521d08c37ee2308c15439d3a49338871a74
Cratto Ctrl <karabontlatleng216@gmail.com>
2026-09-13 06:31:01 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // set base to '/<repo>/'. If using a custom domain or Vercel/Netlify, leave it as '/'.
 export default defineConfig({
   plugins: [react()],
-  base: '/hustle-hard/',
+  base: '/Hustle-Hard-1022/',
 })
